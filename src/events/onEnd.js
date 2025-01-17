45146f7d9cc3951c12695e454bd5566acbc4ebd9
EuiJoong <euijki87@gmail.com>
2025-01-17 09:38:42 +0900

    update 불필요 부분 주석 처리

--- src/events/onEnd.js
+++ src/events/onEnd.js
@@ -1,11 +1,12 @@
-import { removeUser } from '../session/user.session.js';
 import { gameSessions, userSessions } from '../session/sessions.js';
+import saveUserLocation from "../utils/game/saveUserLocation.js";
 
-export const onEnd = (socket) => () => {
-  console.log('클라이언트 연결이 종료되었습니다.');
+export const onEnd = (socket) => async (data) => {
+  console.log('클라이언트 연결이 종료(END)되었습니다.');
+
+  // 유저의 마지막 위치 정보 저장
+  await saveUserLocation(socket);
 
   console.log(userSessions);
   console.log(gameSessions);
-
-  removeUser(socket);
 };
--- src/events/onError.js
+++ src/events/onError.js
@@ -1,10 +1,15 @@
 import { handleError } from '../utils/error/errorHandler.js';
-import { removeUser } from '../session/user.session.js';
+import {getUserBySocket, removeUser} from '../session/user.session.js';
 import CustomError from '../utils/error/customError.js';
+import { onEnd } from './onEnd.js';
+import {findUserLastLocationByUserId} from "../db/user/user.db.js";
+import saveUserLocation from "../utils/game/saveUserLocation.js";
 
-export const onError = (socket) => (err) => {
-  console.error('소켓 오류:', err);
+export const onError = (socket) => async (err) => {
+
   handleError(socket, new CustomError(500, `소켓 오류: ${err.message}`));
-  // 세션에서 유저 삭제
-  removeUser(socket);
+
+  // 유저의 마지막 위치 정보 저장
+  // await saveUserLocation(socket);
+
 };
