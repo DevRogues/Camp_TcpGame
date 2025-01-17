6d34c3a016559f2a12c4df012225f3b97156ab5a
EuiJoong <euijki87@gmail.com>
2025-01-17 12:34:42 +0900

    fix try문 추가

--- src/events/onEnd.js
+++ src/events/onEnd.js
@@ -5,7 +5,12 @@ export const onEnd = (socket) => async (data) => {
   console.log('클라이언트 연결이 종료(END)되었습니다.');
 
   // 유저의 마지막 위치 정보 저장
-  await saveUserLocation(socket);
+  try{
+    await saveUserLocation(socket);
+  }catch (e) {
+    console.error(e);
+  }
+
 
   console.log(userSessions);
   console.log(gameSessions);
