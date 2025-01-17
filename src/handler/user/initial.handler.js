5ded1b0cf86baccfad008fe834c39b04a91d4a48
EuiJoong <euijki87@gmail.com>
2025-01-17 09:51:43 +0900

    update 마지막 위치 조회 추가

--- src/handler/user/initial.handler.js
+++ src/handler/user/initial.handler.js
@@ -2,12 +2,20 @@ import { addUser, getUserById } from '../../session/user.session.js';
 import { HANDLER_IDS, RESPONSE_SUCCESS_CODE } from '../../constants/handlerIds.js';
 import { createResponse } from '../../utils/response/createResponse.js';
 import { handleError } from '../../utils/error/errorHandler.js';
-import { createUser, findUserByDeviceID, updateUserLogin } from '../../db/user/user.db.js';
+import {
+  createUser,
+  findUserByDeviceID,
+  findUserLastLocationByUserId,
+  updateUserLogin
+} from '../../db/user/user.db.js';
+import {getAllGameSessions} from "../../session/game.session.js";
+import CustomError from "../../utils/error/customError.js";
+import {ErrorCodes} from "../../utils/error/errorCodes.js";
+import {userSessions} from "../../session/sessions.js";
 
 const initialHandler = async ({ socket, userId, payload }) => {
   try {
-    const { deviceId } = payload;
-
+    const { deviceId , latency , speed } = payload;
     let user = await findUserByDeviceID(deviceId);
 
     if (!user) {
@@ -16,13 +24,30 @@ const initialHandler = async ({ socket, userId, payload }) => {
       await updateUserLogin(user.id);
     }
 
-    addUser(user.id, socket);
+    const userSessions = getUserById(user.id);
+    if(userSessions){
+      throw new CustomError(ErrorCodes.ALREADY_LOGIN, '로그인한 상태입니다.');
+    }
 
+    //userSession에 해당 유저 추가추가
+    user = addUser(user.id, socket,speed, latency , deviceId);
+
+    //유저의 마지막 위치 정보 조회
+    const userLocation = await findUserLastLocationByUserId(user.id);
+
+    //게임 세션 참여
+    const gameSession = getAllGameSessions();
+
+    if (!gameSession) {
+      throw new CustomError(ErrorCodes.GAME_NOT_FOUND, '게임 세션을 찾을 수 없습니다.');
+    }
+    // 게임 세션참여
+    gameSession[0].addUser(user, socket);
     // 유저 정보 응답 생성
     const initialResponse = createResponse(
       HANDLER_IDS.INITIAL,
       RESPONSE_SUCCESS_CODE,
-      { userId: user.id },
+      { userId: deviceId , x : (userLocation) ? userLocation.x : 0, y : (userLocation) ? userLocation.y :0},
       deviceId,
     );
 
