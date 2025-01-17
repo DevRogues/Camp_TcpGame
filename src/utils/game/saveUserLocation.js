b8dc3adc74d889d10d88a11bb5c34a39d235f8a8
EuiJoong <euijki87@gmail.com>
2025-01-17 12:38:47 +0900

    fix 수정

--- src/utils/game/saveUserLocation.js
+++ src/utils/game/saveUserLocation.js
@@ -9,24 +9,21 @@ const saveUserLocation = async (socket) =>{
     // 해당 소켓으로 유저 정보 조회
     const userSession = getUserBySocket(socket);
 
-    // DB에 유저의 마지막 위치 정보 조회
-    const userDB = await findUserLastLocationByUserId(userSession.id);
-
-    // 데이터가 없으면 생성, 있으면 수정
-
+    if(userSession){
+        // DB에 유저의 마지막 위치 정보 조회
+        const userDB = await findUserLastLocationByUserId(userSession.id);
+        // 데이터가 없으면 생성, 있으면 수정
         if(!userDB) {
             await createUserLastLocation(userSession.id,userSession.x,userSession.y)
         }else{
             await updateUserLastLocation(userSession.id,userSession.x,userSession.y)
         }
-
         // 유저 세션에서 해당 유저 삭제
         removeUser(socket);
-
         // 게임 세션에 유저 정보 삭제
         const gameSession = getAllGameSessions();
-        if(userSession){
-            gameSession[0].removeUser(userSession.id);
+
+        gameSession[0].removeUser(userSession.id);
         }
     }catch (e) {
         throw new CustomError(ErrorCodes.DATABASE_ERROR, e.message);
